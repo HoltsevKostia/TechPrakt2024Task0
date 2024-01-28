fec005f9a3fff66e1e8bde50f24f59aad231b07b
Костянтин Гольцев <ipz224_gko@student.ztu.edu.ua>
2024-01-28 19:16:53 +0200

    Console app added

---
+++ ConsoleAppPrakt/ConsoleAppPrakt/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleAppPrakt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;          
+            int.TryParse(Console.ReadLine(), out a);
+            int.TryParse(Console.ReadLine(), out b);
+            Console.WriteLine($"Sum = {a + b}");
+        }
+    }
+}
